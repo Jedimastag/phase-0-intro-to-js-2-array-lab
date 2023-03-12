8140b2bf7ada5ce2958f7f613bf0ef07b11e027a
George Irwin <jedimastag0@gmail.com>
2023-03-11 20:09:32 -0500

    completes array lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return appendCat = [...cats, "Broom"];
+}
+
+function prependCat(name) {
+    return prependCat = ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+    return removeLastCat = cats.slice(0, 2);
+}
+
+function removeFirstCat() {
+    return removeFirstCat = cats.slice(1, 3);
+}
